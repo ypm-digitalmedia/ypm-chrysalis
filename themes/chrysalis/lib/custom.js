327f4bc242b2f66c34ed81686df342700d11b9c1
Andy Melien <ypm-digitalmedia@gmail.com>
2019-07-16 10:07:32 -0400

    jul 15 work - layouts

--- themes/chrysalis/lib/custom.js
+++ themes/chrysalis/lib/custom.js
@@ -4,68 +4,70 @@ var fliplock = false;
 var fliplockTimeout = null;
 
 
-$(document).ready(function(){
-    
+$(document).ready(function() {
+
     $(".box-item").each(function() {
         var card = $(this).attr("id");
         fliplocks[card] = false;
     });
-    
-   $(".box-item").mouseover(function() {
+
+    $(".box-item").mouseover(function() {
         fadeToBack($(this));
-    }); 
-   
-    
+    });
+
+
     $("body").on("tap", function(event) {
 
         var target = $(event.target);
-//        console.log( target.parents() )
+        //        console.log( target.parents() )
         if (target.not(".card-row") || target.parents('.card-row').length < 1) {
             locked_card = null;
             fadeToFront();
         }
 
-    })
-    
-    
-    
-    
-    
-    
-    
-    
+    });
+
+    $("#closeSearchBox").click(function() {
+        $(".ypm-search-container").slideUp();
+    });
+
+
+
+
+
+
 });
 
-    // =================================== FADE ANIMATIONS - CURRENT =========================================
+// =================================== FADE ANIMATIONS - CURRENT =========================================
 
-    var fadeToBack = _.throttle(function(e) {
+var fadeToBack = _.throttle(function(e) {
 
-        var card = $(e).attr("id");
+    var card = $(e).attr("id");
 
-        if (locked_card == card) {
-            // console.log("DENIED | locked card: " + card);
-        } else {
-            locked_card = card;
-            // console.log("SWITCHING locked card: " + card);
+    if (locked_card == card) {
+        // console.log("DENIED | locked card: " + card);
+    } else {
+        locked_card = card;
+        // console.log("SWITCHING locked card: " + card);
 
-            $(e).addClass("shadow");
-            $(e).addClass("faded");
-            $(e).find(".front").css("opacity", 0);
-            $(e).find(".back").css("opacity", 1);
+        $(e).addClass("shadow");
+        $(e).addClass("faded");
+        $(e).find(".front").css("opacity", 0);
+        $(e).find(".back").css("opacity", 1);
 
-            $(".faded").not(e).find(".front").css("opacity", 1);
-            $(".faded").not(e).find(".back").css("opacity", 0);
-            $(".faded").not(e).removeClass("shadow");
+        $(".faded").not(e).find(".front").css("opacity", 1);
+        $(".faded").not(e).find(".back").css("opacity", 0);
+        $(".faded").not(e).removeClass("shadow");
 
-        }
-
-    }, 100);
-
-    function fadeToFront() {
-        $(".faded").find(".front").css("opacity", 1);
-        $(".faded").find(".back").css("opacity", 0);
-        $(".faded").removeClass("shadow");
     }
 
+}, 100);
 
-    // =============================================================================================
+function fadeToFront() {
+    $(".faded").find(".front").css("opacity", 1);
+    $(".faded").find(".back").css("opacity", 0);
+    $(".faded").removeClass("shadow");
+}
+
+
+// =============================================================================================
